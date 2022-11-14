8064d1528a58b07ced22c3f227138252e5fa3288
Dasha <dasha.ved93@gmail.com>
2022-11-14 14:19:19 +0300

    + correct cart and wishlist

--- routs.py
+++ routs.py
@@ -55,10 +55,16 @@ def main():
 @app.route('/catalog/<parent_id>')
 def get_catalog(parent_id):
     if parent_id:
+        products = []
+        products_id = db.select('SELECT product_id FROM product_category WHERE category_id=%s', (parent_id,))
+        for product_id in products_id:
+            products += \
+                db.select('SELECT product_id, title, author, price, image FROM product WHERE product_id = %s',
+                          values=(product_id['product_id'],))
         categories = db.select(f'SELECT * FROM category WHERE category_parent_id= %s', (parent_id,))
     else:
         categories = db.select('SELECT * FROM category WHERE category_parent_id is NULL')
-    products = db.select('SELECT * FROM product')
+        products = db.select('SELECT * FROM product')
 
     return render_template('shop/catalog.html', categories=categories, products=products,
                            is_authenticated=is_authenticated())
@@ -67,7 +73,6 @@ def get_catalog(parent_id):
 @app.route('/change_quantity/<int:product_id>', methods=['POST'])
 def change_quantity(product_id):
     quantity = int(request.form.get('quantity'))
-
     if is_authenticated():
         db_cart = db.select('SELECT cart_id FROM cart WHERE user_id = %s', values=(session['id'],))[0]
         if 'check' not in request.form:
@@ -91,54 +96,6 @@ def change_quantity(product_id):
     return redirect(request.referrer)
 
 
-@app.route('/add-to-cart/<int:product_id>', methods=['POST'])
-def add_to_cart(product_id):
-    quantity = int(request.form.get('quantity'))
-
-    if is_authenticated():
-        db_cart = db.select('SELECT cart_id FROM cart WHERE user_id = %s', values=(session['id'],))[0]
-        cart = db.select('SELECT product_id, qty FROM cart_item WHERE cart_id = %s AND product_id= %s',
-                         (db_cart['cart_id'], product_id))
-        if cart:
-            db.update('UPDATE cart_item SET qty = qty + 1 WHERE product_id = %s', values=(product_id,))
-        else:
-            db.insert('INSERT INTO cart_item (cart_id, product_id, qty) VALUES (%s, %s, %s)',
-                      values=(db_cart['cart_id'], product_id, quantity))
-    else:
-        if 'cart' not in session:
-            session['cart'] = []
-
-        cart = session["cart"]
-        item_exists = False
-
-        for index, value in enumerate(cart):
-            if value["product_id"] == product_id:
-                cart[index]["qty"] = quantity
-                item_exists = True
-                break
-
-        if not item_exists:
-            cart.append({'product_id': product_id, 'qty': quantity})
-        session["cart"] = cart
-
-    return redirect(request.referrer)
-
-
-@app.route('/delete-item/<int:cart_id>/<int:product_id>', methods=['POST'])
-def delete_item(product_id, cart_id=None):
-    if is_authenticated():
-        db.delete('DELETE FROM cart_item WHERE cart_id = %s AND product_id= %s', values=(cart_id, product_id,))
-    else:
-        cart = session["cart"]
-        for value in cart:
-            if value["product_id"] == product_id:
-                cart.remove(value)
-                break
-        session["cart"] = cart
-
-    return redirect(request.referrer)
-
-
 @app.route('/product/<book_id>')
 def get_book(book_id):
     book = db.select('SELECT * FROM product WHERE product_id = %s', (book_id,))[0]
@@ -154,6 +111,10 @@ def profile():
         if request.method == 'POST':
             pass
         return render_template('shop/personal/profile.html', is_authenticated=is_auth, user=user, form=form)
+
+
+@app.route('/edit-profile/<int:user_id>', methods=['POST'])
+def edit_profile():
 
 
 @app.route('/personal/order')
@@ -204,7 +165,7 @@ def make_order():
             flash('Заказ успешно сформирован и оплачен')
             order_line(order_id)
             if is_auth:
-                db.delete('DELETE FROM cart_item WHERE cart_id = %s', values=(db_cart, ))
+                db.delete('DELETE FROM cart_item WHERE cart_id = %s', values=(db_cart,))
             else:
                 clear_cart()
     return render_template('shop/personal/order_form.html', total_amount=total_amount, total_count=total_count,
@@ -221,12 +182,11 @@ def wishlist():
         products_id = db.select('SELECT product_id FROM wishlist_item WHERE wishlist_id = %s',
                                 values=(user_wishlist['wishlist_id'],))
         for item in products_id:
-            print(item)
             products += \
                 db.select('SELECT product_id, title, author, series, price, image FROM product WHERE product_id = %s',
                           values=(item['product_id'],))
         return render_template('shop/personal/wishlist.html', user=user, products=products,
-                               wishlist_id=user_wishlist['wishlist_id'])
+                               wishlist_id=user_wishlist['wishlist_id'], is_authenticated=is_auth)
 
 
 @app.route('/add-to-wishlist/<int:product_id>', methods=['POST'])
@@ -250,22 +210,30 @@ def add_to_wishlist(product_id):
 
 @app.route('/delete-wishlist/<int:wishlist_id>/<int:product_id>', methods=['POST'])
 def delete_wishlist(wishlist_id, product_id):
-    db.delete('DELETE FROM wishlist_item WHERE wishlist_id=%s AND product_id=%s', values=(wishlist_id, product_id, ))
+    db.delete('DELETE FROM wishlist_item WHERE wishlist_id=%s AND product_id=%s', values=(wishlist_id, product_id,))
     return redirect(request.referrer)
 
 
 def get_product(products, cart):
     amount = 0
     for item in cart:
+        print(item)
         product = db.select(
             'SELECT product_id, title, author, price, quantity, image FROM product WHERE product_id = %s',
             values=(item['product_id'],))[0]
         repeated_product = list(filter(lambda x: x['product_id'] == product['product_id'], products))
+
         if repeated_product:
-            product['qty'] += item['qty']
+            for rep_product in products:
+                if product['product_id'] == rep_product['product_id']:
+                    rep_product['qty'] += item['qty']
+                    product = rep_product
+                    print('product', rep_product)
         else:
+            print('product', product)
             product['qty'] = item['qty']
             products.append(product)
+        print(products)
         amount += product['price'] * product['qty']
     return amount
 
@@ -274,24 +242,87 @@ def get_product(products, cart):
 def get_cart():
     products = []
     total_amount = 0
-    total_count = 0
     is_auth = is_authenticated()
     if is_auth:
-        db_cart = db.select('SELECT cart_id FROM cart WHERE user_id = %s', values=(session['id'],))[0]
-        cart_item = db.select('SELECT product_id, qty FROM cart_item WHERE cart_id = %s', (db_cart['cart_id'],))
+        db_cart = db.select('SELECT cart_id FROM cart WHERE user_id = %s', values=(session['id'],))[0]['cart_id']
+        cart_item = db.select('SELECT product_id, qty FROM cart_item WHERE cart_id = %s', (db_cart,))
         if cart_item:
-            total_count += len(cart_item)
             total_amount += get_product(products, cart_item)
     if 'cart' in session:
-        total_count += len(session['cart'])
         total_amount += get_product(products, session['cart'])
         if is_auth:
             for item in session['cart']:
                 db.insert('INSERT INTO cart_item (cart_id, product_id, qty) VALUES (%s, %s, %s)',
-                          values=(db_cart['cart_id'], item['product_id'], item['qty']))
+                          values=(db_cart, item['product_id'], item['qty']))
             clear_cart()
-    return render_template('shop/personal/cart.html', products=products, total_count=total_count,
-                           cart_id=db_cart['cart_id'] or None, total_amount=total_amount, is_authenticated=is_auth)
+        else:
+            db_cart = 1
+    return render_template('shop/personal/cart.html', products=products, cart_id=db_cart, total_amount=total_amount,
+                           is_authenticated=is_auth)
+
+
+@app.route('/add-to-cart/<int:product_id>', methods=['POST'])
+def add_to_cart(product_id):
+    quantity = int(request.form.get('quantity'))
+
+    if is_authenticated():
+        db_cart = db.select('SELECT cart_id FROM cart WHERE user_id = %s', values=(session['id'],))[0]
+        cart = db.select('SELECT product_id, qty FROM cart_item WHERE cart_id = %s AND product_id= %s',
+                         (db_cart['cart_id'], product_id))
+        if cart:
+            db.update('UPDATE cart_item SET qty = qty + 1 WHERE cart_id=%s AND product_id = %s',
+                      values=(db_cart['cart_id'], product_id,))
+        else:
+            db.insert('INSERT INTO cart_item (cart_id, product_id, qty) VALUES (%s, %s, %s)',
+                      values=(db_cart['cart_id'], product_id, quantity))
+    else:
+        if 'cart' not in session:
+            session['cart'] = []
+
+        cart = session["cart"]
+        item_exists = False
+        for index, value in enumerate(cart):
+
+            if value["product_id"] == product_id:
+                cart[index]["qty"] = quantity
+                item_exists = True
+                break
+
+        if not item_exists:
+            cart.append({'product_id': product_id, 'qty': quantity})
+        session["cart"] = cart
+
+    return redirect(request.referrer)
+
+
+@app.route('/delete-item/<int:cart_id>/<int:product_id>', methods=['GET', 'POST'])
+def delete_item(cart_id, product_id):
+    if is_authenticated():
+        db.delete('DELETE FROM cart_item WHERE cart_id = %s AND product_id= %s', values=(cart_id, product_id,))
+    else:
+        cart = session["cart"]
+        for value in cart:
+            if value["product_id"] == product_id:
+                cart.remove(value)
+                break
+        session["cart"] = cart
+
+    return redirect(request.referrer)
+
+
+def validation_account_form(first_name, last_name, phone, email, password):
+    phones = db.select('SELECT phone FROM account WHERE phone= %s', values=(phone,))
+    if phones:
+        flash('Аккаунт с таким номером телефона уже существует')
+    elif not re.match(r'^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$', phone):
+        flash('Введите корректный номер телефона')
+    elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+        flash('Введите корректный email')
+    elif not first_name or not last_name or not password:
+        flash('Заполните все поля')
+    else:
+        return True
+    return False
 
 
 @app.route('/registration', methods=['GET', 'POST'])
@@ -315,7 +346,7 @@ def registration():
             flash('Введите корректный номер телефона')
         elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
             flash('Введите корректный email')
-        elif not first_name or not last_name or not password or not email:
+        elif not first_name or not last_name or not password:
             flash('Заполните все поля')
         else:
             db.insert('INSERT INTO account (first_name, last_name, phone, email, password)'
@@ -327,7 +358,7 @@ def registration():
             session['loggedin'] = True
             session['id'] = user['user_id']
             session['email'] = user['email']
-            return redirect(url_for('main'))
+            return redirect(request.referrer)
 
     return render_template('shop/auth/registration.html')
 
@@ -348,6 +379,7 @@ def login():
                 return redirect(url_for('main'))
 
         flash('Неправильный логин или пароль. Попробуйте еще раз.')
+        return redirect(request.referrer)
     return render_template('shop/auth/authorization.html')
 
 
@@ -359,7 +391,7 @@ def logout():
     return redirect(url_for('main'))
 
 
-@app.route('/add-product', methods=['GET', 'POST'])
+@app.route('/admin/add-product', methods=['GET', 'POST'])
 def add_product():
     categories = db.select('SELECT id, value FROM category')
     labels = db.select('SELECT id, value FROM shop_label')
